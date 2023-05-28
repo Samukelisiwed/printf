d076ea5ae7434faf47e4724e1ea1ed1f73d38a3f
Precious <ps_dlamini@ymail.com>
2023-05-28 01:30:35 -0700

    Updated fie

--- _printf.c
+++ _printf.c
@@ -61,7 +61,6 @@ return (printed_chars);
 */
 
 void print_buffer(char buffer[], int *buff_ind)
-
 {
 if (*buff_ind > 0)
 write(1, &buffer[0], *buff_ind);
--- main.h
+++ main.h
@@ -1,6 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -32,6 +31,7 @@ char fmt;
 int (*fn)(va_list, char[], int, int, int, int);
 };
 
+
 /**
 * typedef struct fmt fmt_t - Struct op
 *
@@ -47,7 +47,7 @@ va_list list, char buffer[], int flags, int width, int precision, int size);
 
 /****************** FUNCTIONS ******************/
 
-/* Functions to print chars and strings */
+/* Funtions to print chars and strings */
 int print_char(va_list types, char buffer[],
 int flags, int width, int precision, int size);
 int print_string(va_list types, char buffer[],
@@ -56,7 +56,6 @@ int print_percent(va_list types, char buffer[],
 int flags, int width, int precision, int size);
 
 /* Functions to print numbers */
-
 int print_int(va_list types, char buffer[],
 int flags, int width, int precision, int size);
 int print_binary(va_list types, char buffer[],
@@ -73,29 +72,29 @@ int flags, int width, int precision, int size);
 int print_hexa(va_list types, char map_to[],
 char buffer[], int flags, char flag_ch, int width, int precision, int size);
 
-/* Function to print non-printable characters */
+/* Function to print non printable characters */
 int print_non_printable(va_list types, char buffer[],
 int flags, int width, int precision, int size);
 
-/* Function to print memory address */
+/* Funcion to print memory address */
 int print_pointer(va_list types, char buffer[],
 int flags, int width, int precision, int size);
 
-/* Functions to handle other specifiers */
+/* Funciotns to handle other specifiers */
 int get_flags(const char *format, int *i);
 int get_width(const char *format, int *i, va_list list);
 int get_precision(const char *format, int *i, va_list list);
 int get_size(const char *format, int *i);
 
-/* Function to print string in reverse */
+/*Function to print string in reverse*/
 int print_reverse(va_list types, char buffer[],
 int flags, int width, int precision, int size);
 
-/* Function to print a string in ROT13 */
+/*Function to print a string in rot 13*/
 int print_rot13string(va_list types, char buffer[],
 int flags, int width, int precision, int size);
 
-/* Width handler */
+/* width handler */
 int handle_write_char(char c, char buffer[],
 int flags, int width, int precision, int size);
 int write_number(int is_positive, int ind, char buffer[],
